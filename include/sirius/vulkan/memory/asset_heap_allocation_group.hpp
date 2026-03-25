56bb90c21d0663d13376be98f2f8deaf06c5945d
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2026-03-25 16:45:38 -0700

    Fix render instance being (incorrectly) invalid with 0 asset heap configurations

--- include/sirius/vulkan/memory/asset_heap_allocation_group.hpp
+++ include/sirius/vulkan/memory/asset_heap_allocation_group.hpp
@@ -25,3 +25,16 @@ namespace acma::vk::impl {
 		>::realloc...;
 	};
 }
+
+
+namespace acma::vk::impl {
+	template<sl::size_t N, auto AssetHeapConfigs, typename RenderProcessT>
+	struct asset_heap_allocation_group<N, AssetHeapConfigs, RenderProcessT, sl::index_sequence_type<>> {
+
+		template<sl::index_t J>
+		result<void> realloc(sl::index_constant_type<J>, sl::uint64_t = std::numeric_limits<sl::uint64_t>::max()) noexcept
+		requires(J >= N) {
+			return {};
+		}
+	};
+}
