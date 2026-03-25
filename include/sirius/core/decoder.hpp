d87233ecc5b8c005e1d478478abf6fde2c2f10a7
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2026-03-25 16:44:32 -0700

    Move font decoding to its own project
    Since in almost all cases font to texture conversion only needs to be done once, and thus can be done ahead of time, the font decoding functionality has been moved to a separate CLI project: [font2mtsdf](https://github.com/spearglaive/font2mtsdf)

--- include/sirius/core/decoder.hpp
+++ include/sirius/core/decoder.hpp
@@ -35,21 +35,5 @@ namespace acma {
 	namespace decoder { 
 		result<texture>
 		decode_texture(llfio::mapped_file_handle const& handle, texture_usage usage) noexcept;
- 
-		result<std::vector<std::array<std::byte, font_texture::size_bytes>>>
-		decode_font(llfio::mapped_file_handle const& handle) noexcept;
-		
 	};
-}
-
-namespace acma::impl {
-    struct glyph_context {
-        pt2d pos;
-        double scale;
-    };
-
-	[[gnu::nonnull]] constexpr void move_to (hb_draw_funcs_t*, void* shape_ctx, hb_draw_state_t*, float target_x, float target_y, void* glyph_ctx) noexcept;
-    [[gnu::nonnull]] constexpr void line_to (hb_draw_funcs_t*, void* shape_ctx, hb_draw_state_t*, float target_x, float target_y, void* glyph_ctx) noexcept;
-    [[gnu::nonnull]] constexpr void quad_to (hb_draw_funcs_t*, void* shape_ctx, hb_draw_state_t*, float control_x, float control_y, float target_x, float target_y, void* glyph_ctx) noexcept;
-    [[gnu::nonnull]] constexpr void cubic_to(hb_draw_funcs_t*, void* shape_ctx, hb_draw_state_t*, float first_control_x, float first_control_y, float second_control_x, float second_control_y, float target_x, float target_y, void* glyph_ctx) noexcept;
 }
